98f1ba383d567c11f218a2ed273dbfbf1c4b53c8
ATwistedJoke <oatis@ucsd.edu>
2026-05-05 11:39:13 -0700

    Imported the npm installs and started work on the expose.js file.

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,12 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const button = document.querySelector("button");
+  const audio = document.querySelector("audio");
+  const select = document.getElementsByClassName("select");
+
+  button.addEventListener("click", playSound => {
+    audio.play(); 
+  });
+  
 }
